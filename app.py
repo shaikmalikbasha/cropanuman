d90abd3ce9cf29e1297ceb96db9618a0a557217e
shaikmalikbasha <shaikmalikbasha1998@gmail.com>
2021-06-25 15:46:10 +0530

    resolved

--- app.py
+++ app.py
@@ -1,29 +1,31 @@
-from flask import Flask, render_template , request
-import pickle
-import numpy as np 
 import os
+import pickle
 
-model = pickle.load(open('classifier_decision_tree.pkl','rb'))
+import joblib
+import numpy as np
+from flask import Flask, render_template, request
 
-picFolder = os.path.join('static','pics')
+# model = pickle.load(open("classifier_decision_tree.pkl", "rb+"))
+model = joblib.load(open("classifier_decision_tree.pkl", "rb+"))
+picFolder = os.path.join("static", "pics")
 
 app = Flask(__name__)
 
-@app.route('/')
 
+@app.route("/")
 def man():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/predict',method=['POST'])
+
+@app.route("/predict", methods=["POST"])
 def home():
-    data1 = request.form['a']
-    data2 = request.form['b']
-    data3 = request.form['c']
-    data4 = request.form['d']
-    arr = np.array([[data1,data2,data3,data4]])
+    data1 = request.form["a"]
+    data2 = request.form["b"]
+    data3 = request.form["c"]
+    data4 = request.form["d"]
+    arr = np.array([[data1, data2, data3, data4]])
     pred = model.predict(arr)
-    return render_template('after.html',data=pred)
-
+    return render_template("after.html", data=pred)
 
 
 if __name__ == "__main__":
--- cropanuman.py
+++ cropanuman.py
@@ -1,72 +1,86 @@
-#Importing Libraries
-import pandas as pd
-import numpy as np
+# Importing Libraries
+import os
+
 import joblib
+import numpy as np
+import pandas as pd
 
-#Importing Dataset
-df=pd.read_csv('F:\FlaskIntroduction\env\cpdata (1).csv')
+file_name = "cp_data.csv"
 
-#Label Encoding the target variable
+# Importing Dataset
+df = pd.read_csv(os.path.join(os.getcwd(), file_name))
+
+print(df.head())
+
+
+# Label Encoding the target variable
 from sklearn.preprocessing import LabelEncoder
-LE=LabelEncoder()
-df['label']= LE.fit_transform(df['label']) 
+
+LE = LabelEncoder()
+df["label"] = LE.fit_transform(df["label"])
 
 print(LE.classes_)
 
-#Separating attributes and target
-X=df.iloc[:,0:4].values
-y=df.iloc[:,-1].values
+# Separating attributes and target
+X = df.iloc[:, 0:4].values
+y = df.iloc[:, -1].values
 
 
-#Splitting the data into training and test set(20% data in test set)
+# Splitting the data into training and test set(20% data in test set)
 from sklearn.model_selection import train_test_split
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=2)
 
-#Standard Scaling the attribute values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2)
+
+# Standard Scaling the attribute values
 from sklearn.preprocessing import StandardScaler
-S=StandardScaler()
-X_train=S.fit_transform(X_train)
-X_test=S.transform(X_test)
+
+S = StandardScaler()
+X_train = S.fit_transform(X_train)
+X_test = S.transform(X_test)
 
 
-#Importing the KNN Model
+# Importing the KNN Model
 from sklearn.neighbors import KNeighborsClassifier
-Knn=KNeighborsClassifier(n_neighbors=5,weights='uniform',algorithm='auto',p=3,metric='minkowski')
+
+Knn = KNeighborsClassifier(
+    n_neighbors=5, weights="uniform", algorithm="auto", p=3, metric="minkowski"
+)
 
 
 from sklearn.tree import DecisionTreeClassifier
-DC=DecisionTreeClassifier(criterion="gini")
+
+DC = DecisionTreeClassifier(criterion="gini")
 
 
-#Fitting data to the model
-Knn.fit(X_train,y_train)
-DC.fit(X_train,y_train)
+# Fitting data to the model
+Knn.fit(X_train, y_train)
+DC.fit(X_train, y_train)
 
 
-#Predicting Test Data
-y_pred1=Knn.predict(X_test)
-y_pred2=DC.predict(X_test)
+# Predicting Test Data
+y_pred1 = Knn.predict(X_test)
+y_pred2 = DC.predict(X_test)
 
 
+# Saving the model
+joblib.dump(Knn, "classifier_knn.pkl")
+joblib.dump(DC, "classifier_decision_tree.pkl")
 
-#Saving the model
-joblib.dump(Knn, 'classifier_knn.pkl')
-joblib.dump(DC, 'classifier_decision_tree.pkl')
-
-#Calculating metrics
+# Calculating metrics
 from sklearn.metrics import accuracy_score
+
 print(accuracy_score(y_test, y_pred1))
 print(accuracy_score(y_test, y_pred2))
 
-#Accuracy of KNN model - 0.8774193548387097
-#Accuracy of Decsion Tree model -0.9064516129032258
+# Accuracy of KNN model - 0.8774193548387097
+# Accuracy of Decsion Tree model -0.9064516129032258
 
 
-temp=int(input())
-hum=int(input())
-ph=int(input())
-rain=int(input())
-knn_pred= Knn.predict([[temp,hum,ph,rain]])
-dc_pred= DC.predict([[temp,hum,ph,rain]]) 
+temp = int(input("Enter temperature value: "))
+hum = int(input("Enter humidity value: "))
+ph = int(input("Enter ph value: "))
+rain = int(input("Enter rainfall value: "))
+knn_pred = Knn.predict([[temp, hum, ph, rain]])
+dc_pred = DC.predict([[temp, hum, ph, rain]])
 print(LE.classes_[knn_pred[0]])
 print(LE.classes_[knn_pred[0]])
